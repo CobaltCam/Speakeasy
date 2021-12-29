68c8bef4d1ce318591c5ad9e6ec02017087b41e8
Cameron McCarty <75648740+CobaltCam@users.noreply.github.com>
2021-12-28 23:16:38 -0600

    Add files via upload

--- dice.py
+++ dice.py
@@ -2,12 +2,12 @@ import random
 
 rolls = []
 
+
 def roll_dice(num_dice: int, num_sides: int):
     roll = 0
-    global rolls
     rolls.clear()
     for i in range(0, num_dice):
-        roll += random.randrange(1, num_sides)
+        roll = random.randrange(1, num_sides)
         rolls.append(roll)
 
     return roll
